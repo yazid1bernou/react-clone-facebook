b78628aa3e4d4d8eadd344b98632903af46bd3a4
unknown <yazid1bernou@gmail.com>
2023-12-25 08:46:50 +0000

    continued wit topbar component

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -3,7 +3,7 @@ import Topbar from "../../components/topbar/Topbar";
 export default function Home () {
     return (
         <div>
-             Home page 
+            
              <Topbar />
             
         </div>
