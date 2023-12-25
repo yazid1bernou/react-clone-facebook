470f3a1d2450cff7adc4701e69ccfb591adee53f
unknown <yazid1bernou@gmail.com>
2023-12-25 15:17:34 +0000

    I completed the Share component

--- src/components/share/Share.jsx
+++ src/components/share/Share.jsx
@@ -1,5 +1,8 @@
 import './share.css';
 import PermMediaIcon from '@mui/icons-material/PermMedia';
+import LabelIcon from '@mui/icons-material/Label';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import EmojiEmotionsIcon from '@mui/icons-material/EmojiEmotions';
 export default function Share() {
   return (
           <div className='share'>
@@ -13,10 +16,23 @@ export default function Share() {
                    <div className="shareBottom">
                       <div className="shareOptions">
                           <div className="shareOption">
-                            <PermMediaIcon  className="shareIcon" />
+                            <PermMediaIcon htmlColor='tomato' className="shareIcon" />
                              <span className='shareOptionText'>Photo or Video </span>
                           </div>
+                          <div className="shareOption">
+                            <LabelIcon htmlColor='blue' className="shareIcon" />
+                             <span className='shareOptionText'>Tag</span>
+                          </div>
+                          <div className="shareOption">
+                            <LocationOnIcon htmlColor='green' className="shareIcon" />
+                             <span className='shareOptionText'>Location</span>
+                          </div>
+                          <div className="shareOption">
+                            <EmojiEmotionsIcon htmlColor='goldenrod' className="shareIcon" />
+                             <span className='shareOptionText'>Feelings </span>
+                          </div>
                       </div>
+                      <button className='shareButton'> Share </button>
                    </div>
                </div>
           </div>
