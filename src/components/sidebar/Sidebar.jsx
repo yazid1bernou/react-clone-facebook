a97f5346a321e9a9877376f39ab150311dba600c
unknown <yazid1bernou@gmail.com>
2023-12-25 10:58:34 +0000

    create structure of sidebar component

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,9 +1,59 @@
 import './sidebar.css';
+import RssFeedIcon from '@mui/icons-material/RssFeed';
+import ChatIcon from '@mui/icons-material/Chat';
+import PlayCircleIcon from '@mui/icons-material/PlayCircle';
+import GroupsIcon from '@mui/icons-material/Groups';
+import BookmarkIcon from '@mui/icons-material/Bookmark';
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
+import WorkOutlineIcon from '@mui/icons-material/WorkOutline';
+import TodayIcon from '@mui/icons-material/Today';
+import AccountBalanceIcon from '@mui/icons-material/AccountBalance';
+
 
 export default function Sidebar() {
   return (
     <div className='sidebar'>
-        Sidebar
+          <div className="sidebarWrapper">
+                 <ul className='sidebarList'>
+                    <li className="sidebarListItem">
+                        <RssFeedIcon className="sidebarIcon" />
+                        <span className="sidebarListItemText">Feed</span>
+                    </li>
+                    <li className="sidebarListItem">
+                        <ChatIcon className="sidebarIcon" />
+                        <span className="sidebarListItemText">Chats</span>
+                    </li>
+                    <li className="sidebarListItem">
+                        <PlayCircleIcon className="sidebarIcon" />
+                        <span className="sidebarListItemText">Videos</span>
+                    </li>
+                    <li className="sidebarListItem">
+                        <GroupsIcon className="sidebarIcon" />
+                        <span className="sidebarListItemText">Groups</span>
+                    </li>
+                    <li className="sidebarListItem">
+                        <BookmarkIcon className="sidebarIcon" />
+                        <span className="sidebarListItemText">Bookmarks</span>
+                    </li>
+                    <li className="sidebarListItem">
+                        <HelpOutlineIcon className="sidebarIcon" />
+                        <span className="sidebarListItemText">Questions</span>
+                    </li>
+                    <li className="sidebarListItem">
+                        <WorkOutlineIcon className="sidebarIcon" />
+                        <span className="sidebarListItemText">Jobs</span>
+                    </li>
+                    <li className="sidebarListItem">
+                        <TodayIcon className="sidebarIcon" />
+                        <span className="sidebarListItemText">Events</span>
+                    </li>
+                    <li className="sidebarListItem">
+                        <AccountBalanceIcon className="sidebarIcon" />
+                        <span className="sidebarListItemText">Courses</span>
+                    </li>
+                 </ul>
+          </div>
+      
     </div>
   )
 }
