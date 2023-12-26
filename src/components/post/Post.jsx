4fc92baa4e2781b8e5e7a281c9ad5a6d588c4151
unknown <yazid1bernou@gmail.com>
2023-12-26 08:50:54 +0000

    I continue wit post component

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -7,7 +7,7 @@ export default function Post() {
              <div className="postWrapper">
                 <div className="postTop">
                      <div className="postTopLeft">
-                        <img className="postProfileImg" src="/assetss/person/1.jpeg" alt="" />
+                        <img className="postProfileImg" src="/assets/person/1.jpeg" alt="" />
                         <span className="postUsername">Bernou Adem</span>
                         <span className="postDate">10 min ago</span>
                      </div>
@@ -16,10 +16,20 @@ export default function Post() {
                      </div>
                 </div>
                 <div className="postCenter">
-
+                    <span className="postText">
+                     Hey ! it's my first post .
+                    </span>
+                    <img  className="postImg" src="/assets/post/1.jpeg" alt="" />
                 </div>
                 <div className="postBottom">
-
+                     <div className="postBottomLeft">
+                          <img src="/assets/like.png" alt="" />
+                          <img src="/assets/heart.png" alt="" />
+                          <span className="postLikeCounter">32 people like it</span>
+                     </div>
+                     <div className="postBottomRight">
+                          <span className="postCommentText">9 comments</span>
+                     </div>
                 </div>
              </div>
         </div>
