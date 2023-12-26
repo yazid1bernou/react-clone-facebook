44ffabc1917fd0a02bc569656a34596018c58a12
unknown <yazid1bernou@gmail.com>
2023-12-26 09:54:12 +0000

    I continue with rightbar component

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -3,7 +3,15 @@ import './rightbar.css';
 export default function Rightbar() {
   return (
       <div className='rightbar'>
-        Rightbar
+            <div className="rightbarWrapper">
+                 <div className="birthdayContainer"></div>
+                 <img  className="birthdayImg" src="/assets/gift.png" alt="" />
+                 <span className="birthdayText">
+                     <b>Adem bernou </b> and <b> 3 others friends </b> have birthday today
+                 </span>
+            </div>
+            <img className="rightbarAd" src="/assets/ad.png" alt="" />
+            <h4 className="rightbarTitle">Online Friends </h4>
       </div>
   )
 }
