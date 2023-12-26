dbbab09fb871a339217cb27b6a1d2c01772d3a2c
unknown <yazid1bernou@gmail.com>
2023-12-26 15:24:29 +0000

    create dummy data for posts

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,7 +1,7 @@
 import './post.css';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 
-export default function Post() {
+export default function Post({post}) {
   return (
         <div className='post'>
              <div className="postWrapper">
@@ -9,7 +9,7 @@ export default function Post() {
                      <div className="postTopLeft">
                         <img className="postProfileImg" src="/assets/person/1.jpeg" alt="" />
                         <span className="postUsername">Bernou Adem</span>
-                        <span className="postDate">10 min ago</span>
+                        <span className="postDate">{post.date}</span>
                      </div>
                      <div className="postTopRight">
                         <MoreVertIcon />
@@ -17,18 +17,18 @@ export default function Post() {
                 </div>
                 <div className="postCenter">
                     <span className="postText">
-                     Hey ! it's my first post .
+                        {post?.desc}
                     </span>
-                    <img  className="postImg" src="/assets/post/1.jpeg" alt="" />
+                    <img  className="postImg" src={post.photo} alt="" />
                 </div>
                 <div className="postBottom">
                      <div className="postBottomLeft">
                           <img className="likeIcon" src="/assets/like.png" alt="" />
                           <img  className="likeIcon" src="/assets/heart.png" alt="" />
-                          <span className="postLikeCounter">32 people like it</span>
+                          <span className="postLikeCounter">{post.like} people like it</span>
                      </div>
                      <div className="postBottomRight">
-                          <span className="postCommentText">9 comments</span>
+                          <span className="postCommentText">{post.comment} comments</span>
                      </div>
                 </div>
              </div>
