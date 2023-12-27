e88067e80b0848c8c747e45c116fbf49665e6f5e
unknown <yazid1bernou@gmail.com>
2023-12-27 13:09:47 +0000

    I continue with Post data

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,14 +1,17 @@
 import './post.css';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
-
+import {Users} from "../../dummyData" ;
 export default function Post({post}) {
+    
   return (
         <div className='post'>
              <div className="postWrapper">
                 <div className="postTop">
                      <div className="postTopLeft">
-                        <img className="postProfileImg" src="/assets/person/1.jpeg" alt="" />
-                        <span className="postUsername">Bernou Adem</span>
+                        <img className="postProfileImg" src={Users.filter( (u) => u.id === post.userId)[0].profilePicture} alt="" />
+                        <span className="postUsername">
+                         {Users.filter( (u) =>u.id === post.userId)[0].username} 
+                        </span>
                         <span className="postDate">{post.date}</span>
                      </div>
                      <div className="postTopRight">
