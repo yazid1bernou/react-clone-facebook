161aa5efb81da3337bfd97b8bf59180c645db425
unknown <yazid1bernou@gmail.com>
2023-12-28 10:19:17 +0000

    create the function likeHandler

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,8 +1,18 @@
 import './post.css';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import {Users} from "../../dummyData" ;
+import { useState } from 'react';
+
+
 export default function Post({post}) {
     
+  const [like , setLike] =  useState(post.like) ;
+  const [isLiked , setIsLiked] =  useState(false);
+
+  const likeHandler = () => {
+      setLike(isLiked ? like - 1 : like + 1)
+      setIsLiked(!isLiked)
+  }
   return (
         <div className='post'>
              <div className="postWrapper">
@@ -26,9 +36,9 @@ export default function Post({post}) {
                 </div>
                 <div className="postBottom">
                      <div className="postBottomLeft">
-                          <img className="likeIcon" src="/assets/like.png" alt="" />
+                          <img className="likeIcon" src="/assets/like.png"  onClick={likeHandler} alt="" />
                           <img  className="likeIcon" src="/assets/heart.png" alt="" />
-                          <span className="postLikeCounter">{post.like} people like it</span>
+                          <span className="postLikeCounter">{like} people like it</span>
                      </div>
                      <div className="postBottomRight">
                           <span className="postCommentText">{post.comment} comments</span>
