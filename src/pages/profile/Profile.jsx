9fad319b21c865d84df343df7a42841cc709452f
unknown <yazid1bernou@gmail.com>
2023-12-28 11:13:45 +0000

    I continue with profile page

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,9 +1,33 @@
 import './profile.css';
+import Topbar from "../../components/topbar/Topbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Feed from "../../components/feed/Feed";
+import Rightbar from "../../components/rightbar/Rightbar";
 
 export default function Profile() {
   return (
-    <div>
-         My  Profile
-    </div>
+   <>
+      <Topbar />
+      <div className="profile">
+
+            <Sidebar />
+            <div className="profileRight">
+              <div className="profileRightTop">
+                <div className="profileCover">
+                  
+                </div>
+                 <img  className="profileImgCover" src="/assets/post/3.jpeg" alt="" />
+                 <img  className="profileUserImg" src="/assets/person/7.jpeg" alt="" />
+              </div>
+              <div className="profileRightBottom">
+                <Feed />
+                <Rightbar />
+              </div>
+             
+            </div>
+            
+
+      </div>
+   </>
   )
 }
