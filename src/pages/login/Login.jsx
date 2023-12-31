a110861e705ccb99475bf08e445d57110ccfe637
unknown <yazid1bernou@gmail.com>
2023-12-31 10:53:39 +0000

    create login page

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,21 @@
+import "./login.css";
+
+export default function Login() {
+  return (
+        <div className='login'>
+            <div className="loginWrapper">
+                 <div className="loginLeft">
+                     <h3 className="loginLogo">YazidSocial</h3>
+                     <span className="loginDesc">
+                        Connect with friends and the word around you on YazidSocial
+                     </span>
+                 </div>
+                 <div className="loginRight">
+                      <div className="loginBox">
+
+                      </div>
+                 </div>
+            </div>  
+        </div>
+  )
+}
